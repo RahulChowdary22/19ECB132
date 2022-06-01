f1b5eb9bbafc88b2366999e1bf22d11b2f499f6c
Rahul.k <101688960+RahulChowdary22@users.noreply.github.com>
2022-06-01 04:53:25 +0000

    output

--- pointer.c
+++ pointer.c
@@ -25,8 +25,9 @@ printf(" The Alphabets are : \n");
     printf("\n\n");
     return(0);
 }
-
+/*output
  Pointer : Print all the alphabets:
 ----------------------------------------
  The Alphabets are : 
  A  B  C  D  E  F  G  H  I  J  K  L  M  N  O  P  Q  R  S  T  U  V  W  X  Y  Z 
+*/
